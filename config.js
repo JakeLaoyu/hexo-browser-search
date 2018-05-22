3606e38e5f44e0ab4a75e64e500e5aaf06e5e103
Jake <yucj@dxy.cn>
2018-05-22 17:36:41 +0800

    feat: 显示缩略content，标注关键字

--- config.js
+++ config.js
@@ -1,5 +1,6 @@
 module.exports = {
   index: 'https://i.jakeyu.top', // 网站首页
   link: 'https://i.jakeyu.top/archives/', // 归档页面地址
-  searchFile: 'localsearch.xml'
+  searchFile: 'localsearch.xml',
+  port: 3500
 }
--- controllers/reptile.js
+++ controllers/reptile.js
@@ -4,7 +4,8 @@ const xml2js = require('xml2js')
 
 exports.reptile = async (ctx, next) => {
   var items = []
-  const searchReg = new RegExp(ctx.query.search)
+  const searchText = ctx.query.search
+  const searchReg = new RegExp(searchText)
   const res = await axios.get(`${Config.index}/${Config.searchFile}`)
 
   const result = await new Promise((resolve, reject) => {
@@ -18,7 +19,19 @@ exports.reptile = async (ctx, next) => {
 
   result.search.entry.forEach(item => {
     if (searchReg.test(item.title[0].toLowerCase()) || searchReg.test(item.content[0]._.toLowerCase())) {
-      items.push(item)
+      if (searchReg.test(item.content[0]._.toLowerCase())) {
+        let index = item.content[0]._.toLowerCase().indexOf(searchText)
+        var searchContent = item.content[0]._.substr(index - 10, 150)
+        searchContent = searchContent.replace(searchReg, '<b class="search__keyword">' + searchText + '</b>')
+      }
+      if (searchReg.test(item.title[0].toLowerCase())) {
+        var searchTitle = item.title[0].toLowerCase().replace(searchReg, '<b class="search__keyword">' + searchText + '</b>')
+      }
+      items.push({
+        ...item,
+        searchContent: searchContent || item.content[0]._.substr(0, 150),
+        searchTitle: searchTitle || item.title[0]
+      })
     }
   })
 
