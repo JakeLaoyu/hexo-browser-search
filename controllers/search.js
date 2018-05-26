378bb6a6974b938b18798b763774069b0c7e75ec
Jake <yucj@dxy.cn>
2018-05-26 18:06:25 +0800

    feat: show search text in title

--- controllers/search.js
+++ controllers/search.js
@@ -213,7 +213,8 @@ exports.reptile = async (ctx, next) => {
   await ctx.render('index', {
     siteIndex: Config.index,
     items: resultItems,
-    search: ctx.query.search || '搜索结果'
+    search: ctx.query.search || '搜索结果',
+    searchText: searchText
   })
 }
 
