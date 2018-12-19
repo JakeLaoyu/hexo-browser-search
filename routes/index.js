83b8d00f34b577884fdda204a6f49469c2e1a441
Jake <Jake.Laoyu@gmail.com>
2018-12-19 23:49:02 +0800

    feat: router

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,8 @@ const router = require('koa-router')()
 const search = require('../controllers/search')
 const Config = require('../config')
 
-router.get(Config.api.search, search.reptile)
-
-router.get(Config.api.cache, search.cache)
+router
+  .get(Config.api.search, search.reptile)
+  .get(Config.api.cache, search.cache)
 
 module.exports = router
